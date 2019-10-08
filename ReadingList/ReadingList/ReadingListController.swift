fc3f2f88ecd6d8890fa41f241674fb53c886a7d7
Jonathan Lehr <jonathan@aboutobjects.com>
2019-10-08 13:44:14 -0500

    Initial version of ReadingList

---
+++ ReadingList/ReadingList/ReadingListController.swift
@@ -0,0 +1,15 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+import UIKit
+
+class ReadingListController: UITableViewController
+{
+    @IBAction func done(unwindSegue: UIStoryboardSegue) {
+        // TODO: sync UI and save data
+    }
+    
+    @IBAction func cancel(unwindSegue: UIStoryboardSegue) {
+        // Intentionally left blank
+    }
+}
